b17335ac5ef40edaf9f8df0da3c3c6eddb580b59
jakabaskara <jaka.baskara30@gmail.com>
2024-02-27 22:10:21 +0700

    Fix JS Main

--- resources/views/admin/layout/main.blade.php
+++ resources/views/admin/layout/main.blade.php
@@ -59,14 +59,14 @@
     </div>
 
     <!-- Javascripts -->
-    <script src="{{ asset('assets/js/main.min.js') }}"></script>
-    <script src="{{ asset('assets/js/main.min.js') }}"></script>
-    <script src="{{ asset('assets/js/main.min.js') }}"></script>
-    <script src="{{ asset('assets/js/main.min.js') }}"></script>
-    <script src="{{ asset('assets/js/main.min.js') }}"></script>
-    <script src="{{ asset('assets/js/main.min.js') }}"></script>
-    <script src="{{ asset('assets/js/custom.js') }}"></script>
-    <script src="{{ asset('assets/js/main.js') }}"></script>
+    <script src="../../assets/plugins/jquery/jquery-3.5.1.min.js"></script>
+    <script src="../../assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+    <script src="../../assets/plugins/perfectscroll/perfect-scrollbar.min.js"></script>
+    <script src="../../assets/plugins/pace/pace.min.js"></script>
+    <script src="../../assets/plugins/apexcharts/apexcharts.min.js"></script>
+    <script src="../../assets/js/main.min.js"></script>
+    <script src="../../assets/js/custom.js"></script>
+    <script src="../../assets/js/pages/dashboard.js"></script>
 </body>
 
 </html>
