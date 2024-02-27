e038e53a0771a122cc0d2c7d53f463543ed48036
v <vendypaulus@gmail.com>
2024-02-27 22:54:11 +0700

    add migration of unit kerja, karyawan, pimpinan, sisa cuti tahunan, sisa cuti panjang, permintaan cuti, and edit of users

---
+++ database/migrations/2024_02_27_151208_create_karyawan_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('karyawan', function (Blueprint $table) {
+            $table->id();
+            $table->string('NIK');
+            $table->string('nama');
+            $table->enum('level',['karpim','karpel']);
+            $table->date('TMT_bekerja');
+            $table->date('tgl_diangkat_staff')->nullable();
+            $table->foreignId('id_unit_kerja')->constrained('unit_kerja');
+            $table->foreignId('id_users')->constrained('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('karyawan');
+    }
+};
---
+++ database/migrations/2024_02_27_154219_create_permintaan_cuti_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('permintaan_cuti', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('id_karyawan')->constrained('karyawan');
+            $table->enum('jenis_cuti',['panjang','tahunan']);
+            $table->string('alasan');
+            $table->string('alamat');
+            $table->enum('status',['disetujui','dibatalkan','pending']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('permintaan_cuti');
+    }
+};
