d960ecec3f6a186f1f70e5fb748b8a91f7fb6951
jakabaskara <jaka.baskara30@gmail.com>
2024-02-29 22:13:57 +0700

    First Component Livewire

---
+++ app/Livewire/Asisten/TablePersetujuanCuti.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Asisten;
+
+use Livewire\Component;
+
+class TablePersetujuanCuti extends Component
+{
+    public function render()
+    {
+        return view('livewire.asisten.table-persetujuan-cuti');
+    }
+}
