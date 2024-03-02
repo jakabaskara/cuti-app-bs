5cd6fdaa0c63da1bd687f40f8647702adcecc829
v <vendypaulus@gmail.com>
2024-03-03 01:19:38 +0700

    manajer edit4 and admin

--- database/migrations/2012_02_29_085900_create_role_table.php
+++ database/migrations/2012_02_29_085900_create_role_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('role', function (Blueprint $table) {
             $table->id();
-            $table->enum('nama_role', ['user', 'kerani', 'asisten', 'gm', 'sevp']);
+            $table->enum('nama_role', ['user', 'kerani', 'asisten', 'gm', 'sevp', 'manajer']);
             $table->timestamps();
         });
     }
