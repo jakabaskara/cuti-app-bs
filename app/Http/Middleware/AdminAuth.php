54fe0f4992e4485d542d3ed78375fb95a1e3b26f
jakabaskara <jaka.baskara30@gmail.com>
2024-03-05 14:19:13 +0700

    Fix Middleware

--- app/Http/Middleware/AdminAuth.php
+++ app/Http/Middleware/AdminAuth.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\User;
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
 class AdminAuth
