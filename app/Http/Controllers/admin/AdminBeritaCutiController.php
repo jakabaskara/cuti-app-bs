ff9df43273ee5d48744445aeba716a8fc6b11d3e
v <vendypaulus@gmail.com>
2024-03-05 17:57:03 +0700

    admin, manajer berita acara controller

--- app/Http/Controllers/admin/AdminBeritaCutiController.php
+++ app/Http/Controllers/admin/AdminBeritaCutiController.php
@@ -3,12 +3,55 @@
 namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\JenisCuti;
+use App\Models\Keanggotaan;
+use App\Models\PermintaanCuti;
+use App\Models\SisaCuti;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AdminBeritaCutiController extends Controller
 {
+    // public function index()
+    // {
+    //     return view('admin.cuti');
+    // }
+
     public function index()
     {
-        return view('admin.cuti');
+        // $idUser = 1;
+        $idUser = Auth::user()->id;
+        $user = User::find($idUser);
+        $idPosisi = $user->karyawan->posisi->id;
+        // $dataPairing = Pairing::getDaftarKaryawanCuti($idUser)->get();
+        $riwayat = PermintaanCuti::getHistoryCuti($idPosisi)->get();
+        $namaUser = $user->karyawan->nama;
+        $jabatan = $user->karyawan->posisi->jabatan;
+        $jenisCuti = JenisCuti::get();
+        $dataPairing = Keanggotaan::getAnggota($idPosisi);
+        $sisaCuti = $dataPairing->each(function ($data) {
+            $data->sisa_cuti_panjang = SisaCuti::where('id_karyawan', $data->id)->where('id_jenis_cuti', 1)->first()->jumlah ?? '0';
+            $data->sisa_cuti_tahunan = SisaCuti::where('id_karyawan', $data->id)->where('id_jenis_cuti', 2)->first()->jumlah ?? '0';
+        });
+
+        $getDisetujui = PermintaanCuti::getDisetujui($idPosisi);
+        $getPending = PermintaanCuti::getPending($idPosisi);
+        $getDitolak = PermintaanCuti::getDitolak($idPosisi);
+        $getKaryawanCuti = PermintaanCuti::getTodayKaryawanCuti($idPosisi);
+
+        return view('admin.cuti', [
+            'dataPairing' => $dataPairing,
+            'riwayats' => $riwayat,
+            'idPosisi' => $idPosisi,
+            'nama' => $namaUser,
+            'jabatan' => $jabatan,
+            'jenisCuti' => $jenisCuti,
+            'sisaCutis' => $sisaCuti,
+            'disetujui' => $getDisetujui,
+            'pending' => $getPending,
+            'ditolak' => $getDitolak,
+            'karyawanCuti' => $getKaryawanCuti,
+        ]);
     }
 }
--- app/Http/Controllers/manajer/ManajerBeritaCutiController.php
+++ app/Http/Controllers/manajer/ManajerBeritaCutiController.php
@@ -3,12 +3,50 @@
 namespace App\Http\Controllers\manajer;
 
 use App\Http\Controllers\Controller;
+use App\Models\JenisCuti;
+use App\Models\Keanggotaan;
+use App\Models\PermintaanCuti;
+use App\Models\SisaCuti;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ManajerBeritaCutiController extends Controller
 {
     public function index()
     {
-        return view('manajer.cuti');
+        // $idUser = 1;
+        $idUser = Auth::user()->id;
+        $user = User::find($idUser);
+        $idPosisi = $user->karyawan->posisi->id;
+        // $dataPairing = Pairing::getDaftarKaryawanCuti($idUser)->get();
+        $riwayat = PermintaanCuti::getHistoryCuti($idPosisi)->get();
+        $namaUser = $user->karyawan->nama;
+        $jabatan = $user->karyawan->posisi->jabatan;
+        $jenisCuti = JenisCuti::get();
+        $dataPairing = Keanggotaan::getAnggota($idPosisi);
+        $sisaCuti = $dataPairing->each(function ($data) {
+            $data->sisa_cuti_panjang = SisaCuti::where('id_karyawan', $data->id)->where('id_jenis_cuti', 1)->first()->jumlah ?? '0';
+            $data->sisa_cuti_tahunan = SisaCuti::where('id_karyawan', $data->id)->where('id_jenis_cuti', 2)->first()->jumlah ?? '0';
+        });
+
+        $getDisetujui = PermintaanCuti::getDisetujui($idPosisi);
+        $getPending = PermintaanCuti::getPending($idPosisi);
+        $getDitolak = PermintaanCuti::getDitolak($idPosisi);
+        $getKaryawanCuti = PermintaanCuti::getTodayKaryawanCuti($idPosisi);
+
+        return view('manajer.cuti', [
+            'dataPairing' => $dataPairing,
+            'riwayats' => $riwayat,
+            'idPosisi' => $idPosisi,
+            'nama' => $namaUser,
+            'jabatan' => $jabatan,
+            'jenisCuti' => $jenisCuti,
+            'sisaCutis' => $sisaCuti,
+            'disetujui' => $getDisetujui,
+            'pending' => $getPending,
+            'ditolak' => $getDitolak,
+            'karyawanCuti' => $getKaryawanCuti,
+        ]);
     }
 }
