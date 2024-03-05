8b3868a10843ec8477f51ff1f7e6a60d05c5de33
v <vendypaulus@gmail.com>
2024-03-05 10:58:17 +0700

    admin edit

--- app/Http/Controllers/admin/AdminDashboardController.php
+++ app/Http/Controllers/admin/AdminDashboardController.php
@@ -5,19 +5,53 @@ namespace App\Http\Controllers\admin;
 use App\Models\Karyawan;
 use App\Models\SisaCutiPanjang;
 use App\Http\Controllers\Controller;
+use App\Models\JenisCuti;
+use App\Models\Keanggotaan;
+use App\Models\PermintaanCuti;
+use App\Models\SisaCuti;
+use App\Models\User;
 use Barryvdh\DomPDF\Facade\Pdf;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AdminDashboardController extends Controller
 {
     public function index()
     {
-        // $karyawans = Karyawan::where('id_unit_kerja', 1)->with('sisaCutiPanjang')->get();
-        // return view('admin.index', [
-        //     'karyawans' => $karyawans,
-        // ]);
+        // $idUser = 1;
+        $idUser = Auth::user()->id;
+        $user = User::find($idUser);
+        $idPosisi = $user->karyawan->posisi->id;
+        // $dataPairing = Pairing::getDaftarKaryawanCuti($idUser)->get();
+        $riwayat = PermintaanCuti::getHistoryCuti($idPosisi)->get();
+        $namaUser = $user->karyawan->nama;
+        $jabatan = $user->karyawan->posisi->jabatan;
+        $jenisCuti = JenisCuti::get();
+        $dataPairing = Keanggotaan::getAnggota($idPosisi);
+        $sisaCuti = $dataPairing->each(function ($data) {
+            $data->sisa_cuti_panjang = SisaCuti::where('id_karyawan', $data->id)->where('id_jenis_cuti', 1)->first()->jumlah ?? '0';
+            $data->sisa_cuti_tahunan = SisaCuti::where('id_karyawan', $data->id)->where('id_jenis_cuti', 2)->first()->jumlah ?? '0';
+        });
 
-        return view('admin.index');
+        $getDisetujui = PermintaanCuti::getDisetujui($idPosisi);
+        $getPending = PermintaanCuti::getPending($idPosisi);
+        $getDitolak = PermintaanCuti::getDitolak($idPosisi);
+        $getKaryawanCuti = PermintaanCuti::getTodayKaryawanCuti($idPosisi);
+
+        return view('kerani.index', [
+            'dataPairing' => $dataPairing,
+            'riwayats' => $riwayat,
+            'idPosisi' => $idPosisi,
+            'nama' => $namaUser,
+            'jabatan' => $jabatan,
+            'jenisCuti' => $jenisCuti,
+            'sisaCutis' => $sisaCuti,
+            'disetujui' => $getDisetujui,
+            'pending' => $getPending,
+            'ditolak' => $getDitolak,
+            'karyawanCuti' => $getKaryawanCuti,
+
+        ]);
     }
 
     public function downloadPermintaanCutiPDF()
