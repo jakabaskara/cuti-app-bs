37d6fef60a3e5ee64ee0c220831c568e87cbd6f0
v <vendypaulus@gmail.com>
2024-03-05 18:30:46 +0700

    edit kabag controller, kabag index, index + sidebar + app header + controller for admin

--- app/Http/Controllers/kabag/KabagDashboardController.php
+++ app/Http/Controllers/kabag/KabagDashboardController.php
@@ -9,9 +9,12 @@ use App\Models\Karyawan;
 use App\Models\Keanggotaan;
 use App\Models\Pairing;
 use App\Models\PermintaanCuti;
+use App\Models\RiwayatCuti;
 use App\Models\SisaCuti;
 use App\Models\User;
+use Barryvdh\DomPDF\Facade\Pdf;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class KabagDashboardController extends Controller
 {
@@ -21,7 +24,7 @@ class KabagDashboardController extends Controller
         // $idUser = 1;
         $idUser = Auth::user()->id;
         $user = User::find($idUser);
-        $idPosisi = $user->karyawan->id_posisi;
+        $idPosisi = $user->karyawan->posisi->id;
         // $dataPairing = Pairing::getDaftarKaryawanCuti($idUser)->get();
         $riwayat = PermintaanCuti::getHistoryCuti($idPosisi)->get();
         $namaUser = $user->karyawan->nama;
@@ -33,13 +36,12 @@ class KabagDashboardController extends Controller
             $data->sisa_cuti_tahunan = SisaCuti::where('id_karyawan', $data->id)->where('id_jenis_cuti', 2)->first()->jumlah ?? '0';
         });
 
-        // dd(Pairing::where('id_atasan', $idPosisi)->with('bawahan')->get());
-        $getDisetujui = PermintaanCuti::getDisetujuiCuti($idPosisi)->count();
-        $getPending = PermintaanCuti::getPendingCuti($idPosisi)->count();
-        $getDitolak = PermintaanCuti::getDibatalkanCuti($idPosisi);
+        $getDisetujui = PermintaanCuti::getDisetujui($idPosisi);
+        $getPending = PermintaanCuti::getPending($idPosisi);
+        $getDitolak = PermintaanCuti::getDitolak($idPosisi);
         $getKaryawanCuti = PermintaanCuti::getTodayKaryawanCuti($idPosisi);
 
-        return view('kabag.index', [
+        return view('kerani.index', [
             'dataPairing' => $dataPairing,
             'riwayats' => $riwayat,
             'idPosisi' => $idPosisi,
@@ -69,6 +71,7 @@ class KabagDashboardController extends Controller
         $idUser = Auth::user()->id;
         $user = User::find($idUser);
         $idPosisi = $user->karyawan->id_posisi;
+        $karyawan = $user->karyawan;
 
         if (strlen($request->tanggal_cuti) != 10) {
             list($startDate, $endDate) = explode(" to ", $request->tanggal_cuti);
@@ -87,21 +90,66 @@ class KabagDashboardController extends Controller
         $isManager = Karyawan::find($request->karyawan)->posisi->role->nama_role == 'manajer' ? true : false;
         $isChecked = $isManager ? 0 : 1;
 
-        PermintaanCuti::create([
-            'id_karyawan' => $validate['karyawan'],
-            'id_jenis_cuti' => $validate['jenis_cuti'],
-            'tanggal_mulai' => $startDate,
-            'tanggal_selesai' => $endDate,
-            'jumlah_hari_cuti' => $validate['jumlah_cuti'],
-            'alamat' => $validate['alamat'],
-            'alasan' => $validate['alasan'],
-            'id_posisi_pembuat' => $idPosisi,
-            'is_approved' => 0,
-            'is_rejected' => 0,
-            'is_checked' => $isChecked,
-        ]);
+        DB::transaction(function () use ($validate, $startDate, $endDate, $idPosisi, $isChecked, $karyawan) {
 
+            $permintaanCuti = PermintaanCuti::create([
+                'id_karyawan' => $validate['karyawan'],
+                'id_jenis_cuti' => $validate['jenis_cuti'],
+                'tanggal_mulai' => $startDate,
+                'tanggal_selesai' => $endDate,
+                'jumlah_hari_cuti' => $validate['jumlah_cuti'],
+                'alamat' => $validate['alamat'],
+                'alasan' => $validate['alasan'],
+                'id_posisi_pembuat' => $idPosisi,
+                'is_approved' => 0,
+                'is_rejected' => 0,
+                'is_checked' => $isChecked,
+            ]);
+
+            RiwayatCuti::create([
+                'id_permintaan_cuti' => $permintaanCuti->id,
+                'nama_pembuat' => $karyawan->nama,
+                'jabatan_pembuat' => $karyawan->posisi->jabatan,
+            ]);
+        });
 
         return redirect()->back();
+    }
+
+    public function downloadPermintaanCutiPDF($id)
+    {
+        $permintaanCuti = PermintaanCuti::find($id);
+        $karyawan = $permintaanCuti->karyawan;
+        $pairing = Pairing::where('id_bawahan', $karyawan->id_posisi)->get()->first();
+        $jabatan = $pairing->atasan->jabatan;
+        $nama = $pairing->atasan->karyawan->first()->nama;
+        $sisaCutiPanjang = SisaCuti::where('id_karyawan', $karyawan->id)->where('id_jenis_cuti', 1)->first()->jumlah ?? '0';
+        $sisaCutiTahunan = SisaCuti::where('id_karyawan', $karyawan->id)->where('id_jenis_cuti', 2)->first()->jumlah ?? '0';
+        $cutiPanjangDijalani = 0;
+        $cutiTahunanDijalani = 0;
+        if ($permintaanCuti->id_jenis_cuti == 1) {
+            $cutiPanjangDijalani = $permintaanCuti->jumlah_hari_cuti;
+        } else {
+            $cutiTahunanDijalani = $permintaanCuti->jumlah_hari_cuti;
+        }
+
+        $cutiPanjangDijalani += $sisaCutiPanjang;
+        $cutiTahunanDijalani += $sisaCutiTahunan;
+
+        $pdf = Pdf::loadView('form', [
+            'karyawan' => $karyawan,
+            'namaAtasan' => $nama,
+            'jabatan' => $jabatan,
+            'permintaanCuti' => $permintaanCuti,
+            'sisaCutiPanjang' => $sisaCutiPanjang,
+            'sisaCutiTahunan' => $sisaCutiTahunan,
+            'cutiPanjangDijalani' => $cutiPanjangDijalani,
+            'cutiTahunanDijalani' => $cutiTahunanDijalani,
+
+        ]);
+
+        // return view('form');
+
+        return $pdf->download('Form Cuti ' . $karyawan->nama . ' .pdf');
     }
 }
