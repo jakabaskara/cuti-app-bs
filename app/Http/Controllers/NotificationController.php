f814c8f6218c015a909444fdf7340c730526187d
jakabaskara <jaka.baskara30@gmail.com>
2024-03-12 13:46:39 +0700

    Install Telegram SDK

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -14,9 +14,31 @@ class NotificationController extends Controller
     public function sendNotification()
     {
         $user = Auth::user();
-        $message = 'Notification Send Back';
-        Notification::send($user, new SendNotification($message));
+        $message = 'Kirimkan saya chat id';
+        $keyboard = [
+            'inline_keyboard' => [
+                [
+                    ['text' => 'Setujui', 'callback_data' => 'tombol1_data'],
+                    ['text' => 'Tolak', 'callback_data' => 'tombol2_data']
+                ]
+            ]
+        ];
+
+        $pesan = 'Apakah Cuti Disetujui?';
+
+        // Mengonversi keyboard menjadi JSON
+        $keyboard = json_encode($keyboard);
+
+        // Kirim pesan dengan keyboard inline
+        $response = file_get_contents("https://api.telegram.org/bot7168138742:AAH7Nlo0YsgvIl4S-DexMsWK34_SOAocfqI/sendMessage?chat_id=1176854977&text=$message&reply_markup=$keyboard");
 
         return redirect()->back();
+    }
+
+    public function replyNotification($chatId)
+    {
+        $message = 'Notification Send Back';
+
+        $response = file_get_contents("https://api.telegram.org/bot7168138742:AAH7Nlo0YsgvIl4S-DexMsWK34_SOAocfqI/sendMessage?chat_id=1176854977&text=$message");
     }
 }
