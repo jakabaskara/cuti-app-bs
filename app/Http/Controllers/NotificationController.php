2c44e55dd68a8f75e65d575e731d591ff1bee138
jakabaskara <jaka.baskara30@gmail.com>
2024-03-12 15:25:02 +0700

    Fix Bot

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -87,7 +87,7 @@ class NotificationController extends Controller
             if (strtolower($text) === 'sisa cuti') {
                 Telegram::sendMessage([
                     'chat_id' => $chat_id,
-                    'text' => '',
+                    'text' => 'Daftar Sisa Cuti',
                 ]);
             }
         }
