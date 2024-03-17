c04730df54b495a9662c0bcb8cff29cfcdb1f757
jakabaskara <jaka.baskara30@gmail.com>
2024-03-17 14:46:37 +0700

    Change Migration

--- database/migrations/2012_02_29_085900_create_role_table.php
+++ database/migrations/2012_02_29_085900_create_role_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('role', function (Blueprint $table) {
             $table->id();
-            $table->enum('nama_role', ['user', 'kerani', 'asisten', 'gm', 'sevp', 'manajer', 'kabag', 'admin']);
+            $table->enum('nama_role', ['user', 'kerani', 'asisten', 'gm', 'brm', 'manajer', 'kabag', 'admin']);
             $table->timestamps();
         });
     }
