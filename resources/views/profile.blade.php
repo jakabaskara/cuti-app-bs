dbd8d3a86323a57e8bec0042cd2cc9a4b6a25314
v <vendypaulus@gmail.com>
2024-03-26 10:02:48 +0700

    profile and password without modal

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,102 @@
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="description" content="Responsive Admin Dashboard Template" />
+    <meta name="keywords" content="admin,dashboard" />
+    <meta name="author" content="stacks" />
+    <!-- The above 6 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+    <!-- Title -->
+    <title>Neptune - Responsive Admin Dashboard Template</title>
+
+    <!-- Styles -->
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
+        rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;300;400;500;600;700;800&display=swap"
+        rel="stylesheet" />
+    <link
+        href="https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined|Material+Icons+Two+Tone|Material+Icons+Round|Material+Icons+Sharp"
+        rel="stylesheet" />
+    <link href="../../assets/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="../../assets/plugins/perfectscroll/perfect-scrollbar.css" rel="stylesheet" />
+    <link href="../../assets/plugins/pace/pace.css" rel="stylesheet" />
+    <link href="../../assets/plugins/highlight/styles/github-gist.css" rel="stylesheet" />
+    <link href="../../assets/plugins/select2/css/select2.min.css" rel="stylesheet" />
+
+    <!-- Theme Styles -->
+    <link href="../../assets/css/main.min.css" rel="stylesheet" />
+    <link href="../../assets/css/custom.css" rel="stylesheet" />
+
+    <link rel="icon" type="image/png" sizes="32x32" href="../../assets/images/neptune.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="../../assets/images/neptune.png" />
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+    <div class="card">
+        <div class="card-body">
+            <div class="settings-security-two-factor">
+                <h5>Ubah Profile</h5>
+            </div>
+            <div class="row m-t-lg">
+                <div class="col-md-6">
+                    <label for="settingsInputNIK" class="form-label">NIK</label>
+                    <input type="text" class="form-control" id="settingsInputNIK" aria-describedby="settingsNIKHelp"
+                        placeholder="1300xxxx" />
+                </div>
+                <div class="col-md-6">
+                    <label for="settingsPhoneNumber" class="form-label">Nomor HP</label>
+                    <input type="text" class="form-control" id="settingsPhoneNumber" placeholder="(xxx) xxx-xxxx" />
+                </div>
+            </div>
+            <div class="row m-t-lg">
+                <div class="col-md-6">
+                    <label for="settingsInputNama" class="form-label">Nama</label>
+                    <input type="text" class="form-control" id="settingsInputNama" placeholder="John" />
+                </div>
+                <div class="col-md-6">
+                    <label for="settingsInputJabatan" class="form-label">Jabatan</label>
+                    <input type="text" class="form-control" id="settingsInputJabatan"
+                        placeholder="Asisten Tata Usaha & Keuangan" />
+                </div>
+            </div>
+            <div class="row m-t-lg">
+                <div class="col-md-6">
+                    <label for="settingsInputTMT" class="form-label">TMT Bekerja</label>
+                    <input type="text" class="form-control" id="settingsInputTMT" aria-describedby="settingsTMTHelp"
+                        placeholder="2001-01-22" />
+                </div>
+                <div class="col-md-6">
+                    <label for="settingsstaf" class="form-label">Tanggal diangkat Staf</label>
+                    <input type="text" class="form-control" id="settingsstaf" placeholder="2006-11-22" />
+                </div>
+            </div>
+
+            <div class="row m-t-lg">
+                <div class="col">
+                    <a href="#" class="btn btn-primary m-t-sm">Update</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Javascripts -->
+    <script src="../../assets/plugins/jquery/jquery-3.5.1.min.js"></script>
+    <script src="../../assets/plugins/bootstrap/js/popper.min.js"></script>
+    <script src="../../assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+    <script src="../../assets/plugins/perfectscroll/perfect-scrollbar.min.js"></script>
+    <script src="../../assets/plugins/pace/pace.min.js"></script>
+    <script src="../../assets/plugins/highlight/highlight.pack.js"></script>
+    <script src="../../assets/plugins/select2/js/select2.full.min.js"></script>
+    <script src="../../assets/js/main.min.js"></script>
+    <script src="../../assets/js/custom.js"></script>
+    <script src="../../assets/js/pages/settings.js"></script>
+</body>
