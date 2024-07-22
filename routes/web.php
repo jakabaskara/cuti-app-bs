b60c157cdb23b8024bb99cb4c76c9d19ca85b3a3
Vendy Paulus <vendypaulus@gmail.com>
2024-07-23 01:17:12 +0700

    repair excel10

--- routes/web.php
+++ routes/web.php
@@ -59,7 +59,8 @@ Route::group(['prefix' => 'admin', 'middleware' => ['admin.auth']], function () 
 
     // Riwayat Cuti
     Route::get('/riwayat-cuti', [AdminRiwayatCutiController::class, 'index'])->name('riwayat-cuti.index');
-    Route::get('admin/riwayat/export', [AdminRiwayatCutiController::class, 'export'])->name('admin.riwayat.export');
+    // Route::get('admin/riwayat/export', [AdminRiwayatCutiController::class, 'export'])->name('admin.riwayat.export');
+    Route::get('/admin/admin/riwayat/export', [CutiController::class, 'export'])->name('export.riwayat');
 });
 
 
